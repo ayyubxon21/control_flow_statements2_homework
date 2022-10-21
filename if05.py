297b2d616359ef4c9971a6e178d10d072b1d3d07
ayyubxon <ayyubxonhamdamovtatu@gmail.com>
2022-10-20 20:28:49 -0700

    5

--- if05.py
+++ if05.py
@@ -6,10 +6,5 @@ def main(n):
     Returns:
         int: return answer.
     """
-    a = n%10
-    b = n//10%10
-    c = n//100//10%10
-    d = n//1000%10
-    e = n//10000
-    return a
-print(main(23546))
+    return  max(n%10,n//10%10,n//1000%10,n//10000,(n//100%10))
+print(main(12345))
